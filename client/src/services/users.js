d11fc52222038c45d761aa1113672c37bb78bab7
egladoun <egladoun@outlook.com>
2021-10-17 17:47:09 -0400

    added users.js and session.js

---
+++ client/src/services/users.js
@@ -0,0 +1,12 @@
+const list = {
+    {
+        firstName: 'John',
+        lastName: 'Jackson',
+        handle: @jj,
+        password: '12345',
+        isAdmin: true,
+        emails: [
+            "john@jackson.com"
+        ]
+    }
+}
